89a487b5a2d0b816d7b5d8476068e352dcdd5186
Yash Gorana <yash.gorana@hotmail.com>
2025-03-18 12:17:38 +0530

    drop debug cli

--- cmd/test/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/yashgorana/syftbox-go/internal/client"
-
-func main() {
-	client.RejectFile("/Users/yash/SyftBox/datasites/andrew@openmined.org/api_data/pingpong/rpc/ping/8c6aea10-8b64-4e14-8feb-162733fd08d9.request")
-}
