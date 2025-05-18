64bd03eecc0e8a98c171ce8bae71d1cc0684d0c5
Yash Gorana <yash.gorana@hotmail.com>
2025-05-18 21:51:14 +0530

    fix(client/apps): use SHELL & load respective profiles

---
+++ internal/client/apps/app_runner_unix.go
@@ -0,0 +1,41 @@
+//go:build !windows
+
+package apps
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func (a *App) buildCommand(ctx context.Context, runScript string) *exec.Cmd {
+	var command strings.Builder
+
+	shell := os.Getenv("SHELL")
+	if shell == "" {
+		shell = "sh"
+	}
+
+	shellBase := filepath.Base(shell)
+
+	switch shellBase {
+	case "zsh":
+		command.WriteString("test -f ~/.zshrc && source ~/.zshrc; ")
+	case "bash":
+		command.WriteString("test -f ~/.bashrc && source ~/.bashrc; ")
+	case "fish":
+		command.WriteString("test -f ~/.config/fish/config.fish && source ~/.config/fish/config.fish; ")
+	default:
+		command.WriteString("test -f ~/.profile && source ~/.profile; ")
+	}
+
+	command.WriteString("chmod +x " + runScript + "; ")
+	command.WriteString("exec " + runScript + ";")
+
+	slog.Debug("running app", "shell", shell, "script", runScript, "command", command.String())
+
+	return exec.CommandContext(ctx, shell, "-l", "-c", command.String())
+}
