d740df1db7e11315ef5236a77b64550c178486e8
Yash Gorana <yash.gorana@hotmail.com>
2025-05-22 11:22:00 +0530

    fix(server): fix cors

--- internal/server/middlewares/cors.go
+++ internal/server/middlewares/cors.go
@@ -1,34 +1,19 @@
 package middlewares
 
 import (
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // server cors config
 var corsConfig = cors.Config{
-	AllowAllOrigins: true,
-	AllowMethods:    []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
-	AllowHeaders: []string{
-		"Origin",
-		"Content-Length",
-		"Content-Type",
-		"Authorization",
-		"X-Syft-Msg-Type",
-		"X-Syft-From",
-		"X-Syft-To",
-		"X-Syft-App",
-		"X-Syft-AppEp",
-		"X-Syft-Method",
-		"X-Syft-Headers",
-		"X-Syft-Status",
-	},
-	AllowCredentials: true,
-	MaxAge:           12 * time.Hour,
+	AllowOrigins:     []string{"*"},
+	AllowHeaders:     []string{"*"},
+	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+	AllowCredentials: false,
+	AllowWebSockets:  true,
 }
 
-func InitCORS() gin.HandlerFunc {
+func CORS() gin.HandlerFunc {
 	return cors.New(corsConfig)
 }
